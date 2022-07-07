1c2f4c57a7b014d174413fe8d2587341fb192e96
Paresh Chouhan <paresh@cumul.io>
2022-07-07 14:25:04 +0530

    Updated python package

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cumulio.cumulio import Cumulio
 from dotenv import load_dotenv
 from flask import Flask
+from flask import jsonify
 import os
 
 app = Flask(__name__)
@@ -8,23 +9,29 @@ app = Flask(__name__)
 
 load_dotenv()
 
-client = Cumulio(os.getenv('CUMUL_KEY'), os.getenv('CUMUL_TOKEN'), "https://api.cumul.io")
+client = Cumulio(os.getenv("CUMUL_KEY"), os.getenv("CUMUL_TOKEN"), os.getenv("API_URL"))
 
 properties = {}
-properties["integration_id"] = os.getenv('INTEGRATION_ID')
+properties["integration_id"] = os.getenv("INTEGRATION_ID")
 properties["type"] = "sso"
 properties["expiry"] = "24 hours"
 properties["inactivity_interval"] = "10 minutes"
-properties["name"] = os.getenv('USER_NAME')
-properties["email"] = os.getenv('USER_EMAIL')
+properties["username"] = os.getenv("USER_USERNAME")
+properties["name"] = os.getenv("USER_NAME")
+properties["email"] = os.getenv("USER_EMAIL")
 properties["suborganization"] = "< user suborganization >"
 properties["role"] = "viewer"
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
   authorization = client.create("authorization", properties)
-  return 'Hello, World!'
+  authResponse = {}
+  authResponse["status"] = "success"
+  authResponse["key"] = authorization["id"]
+  authResponse["token"] = authorization["token"]
 
-if __name__ == '__main__':
+  return jsonify(authResponse)
+
+if __name__ == "__main__":
   app.run()
